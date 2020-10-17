fdf6abec89755e4f458878c008951cfd8b348113
xdebbie <deh@live.in>
2020-10-17 19:47:40 +0200

    Add router

---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+<div class="home-container">
+    <div class="title">
+        <h1>Pacific.</h1>
+        <p>Snowboarding</p>
+        <p>Skiing</p>
+        <p>Bobsled</p>
+        <p>Skeleton</p>
+    </div>
+    <div class="address">
+        <p>1100 Lexington Ave</p>
+        <p>New York</p>
+        <p>NY 10075</p>
+        <p>U.S.A.</p>
+    </div>
+</div>
+</template>
